8692d1904cb1735b0193cdcefa39eaf611b35250
snowgoing <snowgoing@gmail.com>
2016-06-24 14:35:09 -0700

    Under Construction Page

--- src/client/routes/router.js
+++ src/client/routes/router.js
@@ -10,6 +10,7 @@ import App from 'layouts/app';
 // import Homepage from 'ui/home';
 import Home from 'ui/home';
 import Calender from 'ui/calender';
+import UnderConstruction from 'ui/underConstruction';
 
 
 
@@ -17,8 +18,9 @@ export default (
   <Provider store={store}>
     <Router history={browserHistory}>
       <Route component={App}>
-      	<Route path='/' component={Home} />
+      	<Route path='/' component={UnderConstruction} />
       	<Route path='/calender' component={Calender} />
+      	<Route path='/home' component={Home} />      	
       </Route>
     </Router>
   </Provider>
