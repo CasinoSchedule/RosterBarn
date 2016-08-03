46ef57df0733918f6bd8dbda67a008d574811bc8
snowgoing <snowgoing@gmail.com>
2016-08-03 15:36:18 -0700

    Fix Employee Info Model

--- src/client/ui/employeeInfoForm.js
+++ src/client/ui/employeeInfoForm.js
@@ -13,6 +13,7 @@ import MenuItem from 'material-ui/MenuItem';
 
 
 require('assets/styles/employeeInfoForm.scss');
+var $ = require('jquery');
 
 const styles = {
   errorStyle: {
@@ -31,6 +32,7 @@ const styles = {
   },
   block: {
     maxWidth: 40,
+    marginRight: -2
   },
   checkbox: {
     marginBottom: 16,
@@ -38,33 +40,35 @@ const styles = {
 };
 
 const items = [
-  <MenuItem key={1} value={1} primaryText="Grave" />,
-  <MenuItem key={2} value={2} primaryText="Day" />,
-  <MenuItem key={3} value={3} primaryText="Swing" />,
-  <MenuItem key={4} value={4} primaryText="Any" />,
+  <MenuItem key={1} value={1} label="Grave" primaryText="Grave" />,
+  <MenuItem key={2} value={2} label="Day" primaryText="Day" />,
+  <MenuItem key={3} value={3} label="Swing" primaryText="Swing" />,
+  <MenuItem key={4} value={4} label="Any" primaryText="Any" />,
 ];
 
 export default React.createClass({
 	getInitialState: function(){
 		return {
-			phone_number_first: '',
-			phone_number_second: '',
-			phone_number_third: '',
+			phone_number_first: (((this.props.info.phone_number) && this.props.info.phone_number.length >= 3) ?  this.props.info.phone_number.slice(0,3) : ''),
+			phone_number_second: (((this.props.info.phone_number) && this.props.info.phone_number.length >= 6) ?  this.props.info.phone_number.slice(3,6) : ''),
+			phone_number_third: (((this.props.info.phone_number) && this.props.info.phone_number.length === 10) ?  this.props.info.phone_number.slice(6,10) : ''),
 			showDeleteConfirm: false,
-			value: null
+			value: (((this.props.info.availability) && this.props.info.availability.length > 1) ? 4 : this.props.info.availability[0]),
+			shift: (((this.props.info.availability) && this.props.info.availability.length > 1) ? 4 : this.props.info.availability[0]),
+			daysOff: this.props.info.regular_days_off
 		}
 	},
 	componentWillMount: function(){
 		this.unsubscribe = store.subscribe(function(){
 			var currentStore = store.getState();
 			this.setState({
-				phone_number_first: ((this.props.info.phone_number) ? this.props.info.phone_number.slice(0,3) : ''),
-				phone_number_second: ((this.props.info.phone_number) ? this.props.info.phone_number.slice(3,6) : ''),
-				phone_number_third: ((this.props.info.phone_number) ? this.props.info.phone_number.slice(6,10) : ''),
+				phone_number_first: (((this.props.info.phone_number) && this.props.info.phone_number.length >= 3) ?  this.props.info.phone_number.slice(0,3) : ''),
+				phone_number_second: (((this.props.info.phone_number) && this.props.info.phone_number.length >= 6) ?  this.props.info.phone_number.slice(3,6) : ''),
+				phone_number_third: (((this.props.info.phone_number) && this.props.info.phone_number.length === 10) ?  this.props.info.phone_number.slice(6,10) : ''),
 				showDeleteConfirm: currentStore.showReducer.showDeleteConfirm
 			})
 		}.bind(this));
-		console.log('phone number', this.props.info.phone_number);
+		console.log('phone_number', this.props.info.phone_number);
 	},
 	close: function(){
 		store.dispatch({
@@ -74,9 +78,28 @@ export default React.createClass({
 
 	},
 	handleChange: function(event, index, value){
+		if(value === 4){
+			this.state.shift = [1, 2, 3];
+		} else {
+			this.state.shift = [value];
+		}
+		
 		this.setState({
-			value 
+			value
 		})
+		console.log(this.state.shift);
+	},
+	handleDaysOffCheck: function(e, isInputChecked){
+		var days = this.state.daysOff;
+		var val = parseInt(e.target.id.slice(3,4));
+
+		if(days.indexOf(val) === -1){
+			days.push(val);
+		} else {
+			days.splice(days.indexOf(val), 1);
+		}	
+		
+		console.log(this.state.daysOff)
 	},
 	handleSubmit: function(e){
 		e.preventDefault();
@@ -91,7 +114,8 @@ export default React.createClass({
 			employee_id: this.refs.employee_id.getValue() || "",
 			email: this.refs.email.getValue() || "",
 			phone_number: this.refs.phone_number_1.getValue() + this.refs.phone_number_2.getValue() + this.refs.phone_number_3.getValue()  || "",
-			availability: this.state.value
+			availability: this.state.shift || [],
+			regular_days_off: this.state.daysOff || []
 		});
 
 		console.log(this.props.info.id, {
@@ -101,7 +125,8 @@ export default React.createClass({
 			employee_id: this.refs.employee_id.getValue() || "",
 			email: this.refs.email.getValue() || "",
 			phone_number: this.refs.phone_number_1.getValue() + this.refs.phone_number_2.getValue() + this.refs.phone_number_3.getValue()  || "",
-			availability: this.state.value
+			availability: this.state.shift || [],
+			regular_days_off: this.state.daysOff || []
 		});
 
 		this.props.refreshCurrentState(this.props.currentDate);
@@ -132,6 +157,9 @@ export default React.createClass({
 		})
 		
 	},
+	componentWillUnmount: function(){
+		this.unsubscribe();
+	},
 	render: function(){
 		return (
 			<div>
@@ -151,6 +179,19 @@ export default React.createClass({
 						ref="position_title" 
 						floatingLabelText="Position Title" 
 						defaultValue={this.props.info.position_title}/>
+
+					<div>
+						<SelectField
+					        value={this.state.value}
+					        onChange={this.handleChange}
+					        style={{fontFamily: 'Arial', marginBottom: '-20px'}} 
+					       	floatingLabelText="Shift"
+					    	floatingLabelFocusStyle={styles.floatingLabelFocusStyle} >
+					    	{items}
+				        
+				        </SelectField>
+			        </div>
+			        
 
 					<TextField 
 						id='first_name' 
@@ -192,60 +233,79 @@ export default React.createClass({
 					<div className="phoneNumber">
 						<TextField 
 							id="phone_number_1" 
-							style={{width: '40px', marginBottom: '-20px'}}
+							style={{width: '40px', marginBottom: '-10px'}}
 							ref="phone_number_1" 
-							defaultValue={this.props.info.phone_number.slice(0,3) || ''}
+							defaultValue={this.state.phone_number_first || ''}
+							floatingLabelFocusStyle={styles.floatingLabelFocusStyle} 
 							floatingLabelText="Phone" 
 							maxLength="3"/>&nbsp;
 						
 						<TextField 
 							id="phone_number_2"
-							style={{width: '40px', marginBottom: '-20px'}} 
+							style={{width: '40px', marginBottom: '-10px'}} 
 							ref="phone_number_2" 
-							defaultValue={this.props.info.phone_number.slice(3,6) || ''}
+							defaultValue={this.state.phone_number_second || ''}
 							floatingLabelText="." 
 							maxLength="3"/>&nbsp;
 
 						<TextField 
 							id="phone_number_3" 
-							style={{width: '60px', marginBottom: '-20px'}} 
+							style={{width: '50px', marginBottom: '-10px'}} 
 							ref="phone_number_3" 
-							defaultValue={this.props.info.phone_number.slice(6,10) || ''}
+							defaultValue={this.state.phone_number_third || ''}
 							floatingLabelText="." 
 							maxLength="4"/>
 					</div>
 
-					<SelectField
-				          value={this.state.value}
-				          onChange={this.handleChange}
-				          floatingLabelText="Shift">
-				    	
-				    	{items}
-			        
-			        </SelectField>
 
 					<label htmlFor="regular_days_off" className="labelColor">Regular Days Off</label>
-					<div className="dayLabel">
-						<div>SUN</div>
-						<div>MON</div>
-						<div>TUE</div>
-						<div>WED</div>
-						<div>THU</div>
-						<div>FRI</div>
-						<div>SAT</div>
-					</div>
+					<div className="daysOffContainer">
+						<div className="dayLabel">
+							<div>SUN</div>
+							<div>MON</div>
+							<div>TUE</div>
+							<div>WED</div>
+							<div>THU</div>
+							<div>FRI</div>
+							<div>SAT</div>
+						</div>
 
-					
-					<div className="weekFlex">
-						<Checkbox id='Sun' ref='Sun1' style={styles.block}/>
-						<Checkbox id='Mon' ref='Mon2' style={styles.block}/>
-						<Checkbox id='Tue' ref='Tue3' style={styles.block}/>
-						<Checkbox id='Wed' ref='Wed4' style={styles.block}/>
-						<Checkbox id='Thu' ref='Thu5' style={styles.block}/>
-						<Checkbox id='Fri' ref='Fri6' style={styles.block}/>
-						<Checkbox id='Sat' ref='Sat7' style={styles.block}/>
+						
+						<div className="weekFlex">
+							<Checkbox 
+								id='Sun1' 
+								onCheck={this.handleDaysOffCheck} 
+								style={styles.block} 
+								defaultChecked={((this.props.info.regular_days_off.indexOf(1) !== -1) ? true : false)}/>
+							<Checkbox 
+								id='Mon2' 
+								onCheck={this.handleDaysOffCheck} 
+								style={styles.block}
+								defaultChecked={((this.props.info.regular_days_off.indexOf(2) !== -1) ? true : false)}/>
+							<Checkbox 
+								id='Tue3' 
+								onCheck={this.handleDaysOffCheck} 
+								style={styles.block}
+								defaultChecked={((this.props.info.regular_days_off.indexOf(3) !== -1) ? true : false)}/>
+							<Checkbox 
+								id='Wed4' 
+								onCheck={this.handleDaysOffCheck} 
+								style={styles.block}
+								defaultChecked={((this.props.info.regular_days_off.indexOf(4) !== -1) ? true : false)}/>
+							<Checkbox 
+								id='Thu5' 
+								onCheck={this.handleDaysOffCheck} style={styles.block}
+								defaultChecked={((this.props.info.regular_days_off.indexOf(5) !== -1) ? true : false)}/>
+							<Checkbox 
+								id='Fri6' 
+								onCheck={this.handleDaysOffCheck} style={styles.block}
+								defaultChecked={((this.props.info.regular_days_off.indexOf(6) !== -1) ? true : false)}/>
+							<Checkbox 
+								id='Sat7' 
+								onCheck={this.handleDaysOffCheck} style={styles.block}
+								defaultChecked={((this.props.info.regular_days_off.indexOf(7) !== -1) ? true : false)}/>
+						</div>
 					</div>
-
 						
 				</div>
 			</div>
