044f406fe45f72a789d18fb042b9467224184269
Aaron Scruggs <aarondscruggs@gmail.com>
2016-08-03 17:13:34 -0700

    Reset cookie during login

--- src/client/api/data.js
+++ src/client/api/data.js
@@ -38,7 +38,8 @@ export function checkAdmin(){
 	// console.log("api", api);
 	// console.log("check_admin", Cookie.get('token'));
 	return api.get('/profiles/check/').then(function(resp){
-		// console.log('checkAdmin function', resp.data.type, resp.data.department, resp.data.department_title);
+		// console.log("After api.get", Cookie.get('token'));
+		console.log('checkAdmin function', resp.data.type, resp.data.department, resp.data.department_title);
 		if(resp.data.type === "manager"){
 			localStorage.setItem("departmentId", resp.data.department);
 			localStorage.setItem("departmentTitle", resp.data.department_title);
